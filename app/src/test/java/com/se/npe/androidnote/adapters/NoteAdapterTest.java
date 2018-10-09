703176019ae0dd7792419980eb8fb9e7b302c3b5
mango <weijd17@mails.tsinghua.edu.cn>
2018-10-09 17:35:53 +0800

    Implement the test of NoteAdapter #5 Mainly on NoteAdapter's implementation of INoteCollection. Suspect that NoteAdapter's insert() has some problems to solve.

--- app/src/test/java/com/se/npe/androidnote/adapters/NoteAdapterTest.java
+++ app/src/test/java/com/se/npe/androidnote/adapters/NoteAdapterTest.java
@@ -1,5 +1,6 @@
 package com.se.npe.androidnote.adapters;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 
 import com.se.npe.androidnote.ListActivity;
@@ -34,11 +35,7 @@ public class NoteAdapterTest {
     public void setUp() {
         this.noteList = new ArrayList<>();
         for (int i = 0; i < NOTE_LIST_SIZE; ++i) {
-            // Depends on the correctness of Note
-            String title = "This is the title for " + String.valueOf(i);
-            List<IData> content = new ArrayList<>();
-            content.add(new TextData("This is the content for " + String.valueOf(i)));
-            noteList.add(new Note(title, content));
+            noteList.add(getExampleNote(i));
         }
         mockActivity = new ListActivity();
         mockNoteAdapter = new NoteAdapter(noteList, mockActivity);
@@ -50,19 +47,63 @@ public class NoteAdapterTest {
 
     @Test
     public void getAllNotes() {
-        assertEquals(mockNoteAdapter.getAllNotes(), this.noteList);
+        assertEquals(this.noteList, mockNoteAdapter.getAllNotes());
     }
 
     @Test
     public void getSearchResult() {
+        // Depends on example note
+        // Whole note list
+        assertEquals(this.noteList, mockNoteAdapter.getSearchResult("title"));
+        // Empty note list
+        assertEquals(new ArrayList<Note>(), mockNoteAdapter.getSearchResult("wtf???"));
+
+        List<Note> noteListSearched = new ArrayList<>();
+        // Depends on setUp()
+        // Search for 3 using old note
+        noteListSearched.add(this.noteList.get(3));
+        noteListSearched.add(this.noteList.get(13));
+        assertEquals(noteListSearched, mockNoteAdapter.getSearchResult("3"));
+        noteListSearched.clear(); // Pay attention to clear noteListSearch
+        // Search for 3 using new note
+        // Not equals because new note has a different data
+        noteListSearched.add(getExampleNote(3));
+        noteListSearched.add(getExampleNote(13));
+        assertNotEquals(noteListSearched, mockNoteAdapter.getSearchResult("3"));
+        noteListSearched.clear();
     }
 
     @Test
     public void addNote() {
+        // Add 5 notes
+        final int NOTE_LIST_SIZE_ADD = 5;
+        for (int i = 0; i < NOTE_LIST_SIZE_ADD; ++i) {
+            Note note = getExampleNote(NOTE_LIST_SIZE + i);
+            mockNoteAdapter.addNote(note);
+            noteList.add(note);
+        }
+        assertEquals(noteList, mockNoteAdapter.getAllNotes());
+        // Add a different note
+        // Not equals
+        mockNoteAdapter.addNote(getExampleNote(NOTE_LIST_SIZE + NOTE_LIST_SIZE_ADD));
+        noteList.add(getExampleNote(NOTE_LIST_SIZE + NOTE_LIST_SIZE_ADD + 1));
+        assertNotEquals(noteList, mockNoteAdapter.getAllNotes());
     }
 
     @Test
     public void getNoteAt() {
+        // All corresponding notes are equal
+        for (int i = 0; i < NOTE_LIST_SIZE; ++i) {
+            assertEquals(noteList.get(i), mockNoteAdapter.getNoteAt(i));
+        }
+        // Not corresponding notes are not equal
+        assertNotEquals(noteList.get(0), mockNoteAdapter.getNoteAt(1));
+        assertNotEquals(noteList.get(10), mockNoteAdapter.getNoteAt(3));
+        // New note is not equal
+        Note note = getExampleNote(NOTE_LIST_SIZE);
+        for (int i = 0; i < NOTE_LIST_SIZE; ++i) {
+            assertNotEquals(note, mockNoteAdapter.getNoteAt(i));
+        }
     }
 
     @Test
@@ -147,5 +188,14 @@ public class NoteAdapterTest {
 
     @Test
     public void onItemDismiss() {
+    }
+
+    @NonNull
+    private Note getExampleNote(int data) {
+        // Create a new example note
+        String title = "This is the title for " + String.valueOf(data);
+        List<IData> content = new ArrayList<>();
+        content.add(new TextData("This is the content for " + String.valueOf(data)));
+        return new Note(title, content);
     }
 }
