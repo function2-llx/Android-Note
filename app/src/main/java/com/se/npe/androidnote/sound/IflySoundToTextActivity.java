3d08a036ab729df236fa7c36f22f1916265764b4
ExfJoe <gwy17@mails.tsinghua.edu.cn>
2018-10-16 23:23:30 +0100

    continue to write IflySoundToTextActivity.java. #15

--- app/src/main/java/com/se/npe/androidnote/sound/IflySoundToTextActivity.java
+++ app/src/main/java/com/se/npe/androidnote/sound/IflySoundToTextActivity.java
@@ -1,12 +1,19 @@
 package com.se.npe.androidnote.sound;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
+import com.iflytek.cloud.ErrorCode;
 import com.iflytek.cloud.InitListener;
 import com.iflytek.cloud.RecognizerListener;
 import com.iflytek.cloud.RecognizerResult;
+import com.iflytek.cloud.SpeechConstant;
 import com.iflytek.cloud.SpeechError;
 import com.iflytek.cloud.SpeechRecognizer;
 
@@ -15,10 +22,11 @@ public class IflySoundToTextActivity extends AppCompatActivity {
     private TextView textView;
     private SpeechRecognizer speechRecognizer;
     private String mEngineType = null;
+    private SharedPreferences sharedPreferences;
     private int result = 0;
 
     @Override
-    protected void onCreat(@nullable Bundle savedInstanceState) {
+    protected void onCreat(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recognition);
         initView();
@@ -26,15 +34,21 @@ public class IflySoundToTextActivity extends AppCompatActivity {
     }
 
     private void initDate() {
-
+        speechRecognizer = SpeechRecognizer.createRecognizer(this, initListener);
+        sharedPreferences = getSharedPreferences(this.getPackageName(), Context.MODE_PRIVATE);
+        mEngineType = SpeechConstant.TYPE_CLOUD;
     }
 
     private void initView() {
-
+        textView = (TextView) findViewById(R.id.tv);
     }
 
-    public void start() {
-
+    public void start(View view) {
+        textView.setText("");
+        result = speechRecognizer.startListening(recognizerListener);
+        if (result != ErrorCode.SUCCESS) {
+            Log.e("resultTag", "error" + result);
+        }
     }
 
     public void stop() {
