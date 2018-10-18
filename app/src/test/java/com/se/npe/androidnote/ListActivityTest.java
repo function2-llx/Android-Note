c5e2022a89c4453f87fd7ad5f8a7e98bab1c3c45
function2-llx <function2@qq.com>
2018-10-18 11:11:47 +0800

    add test for list

---
+++ app/src/test/java/com/se/npe/androidnote/ListActivityTest.java
@@ -0,0 +1,40 @@
+package com.se.npe.androidnote;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(RobolectricTestRunner.class)
+public class ListActivityTest {
+
+    @Test
+    public void onCreateContextMenu() {
+        
+    }
+
+    @Test
+    public void onContextItemSelected() {
+    }
+
+    @Test
+    public void onCreateOptionsMenu() {
+    }
+
+    @Test
+    public void onOptionsItemSelected() {
+    }
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onDestroy() {
+    }
+
+    @Test
+    public void onResume() {
+    }
+}
