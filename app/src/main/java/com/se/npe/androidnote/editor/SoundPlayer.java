9e4da36ccbbf8288b59d97bbffabfb73dacfd8dc
MashPlant <740678788@qq.com>
2018-10-20 00:19:29 +0800

    Fix bug: only one progress bar could be running. Remove the duplicate field in SoundPlayer.

--- app/src/main/java/com/se/npe/androidnote/editor/SoundPlayer.java
+++ app/src/main/java/com/se/npe/androidnote/editor/SoundPlayer.java
@@ -3,6 +3,7 @@ package com.se.npe.androidnote.editor;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
 import android.os.AsyncTask;
 import android.os.Handler;
@@ -29,7 +30,7 @@ import cn.jzvd.JzvdStd;
 public class SoundPlayer extends RelativeLayout {
     private static final String LOG_TAG = SoundPlayer.class.getSimpleName();
 
-    private static class ProgressSetter extends AsyncTask<Void, Integer, Void> {
+    private static class ProgressSetter extends AsyncTask<Void, Void, Void> {
         WeakReference<SoundPlayer> ref;
 
         ProgressSetter(SoundPlayer ref) {
@@ -38,32 +39,26 @@ public class SoundPlayer extends RelativeLayout {
 
         @Override
         protected Void doInBackground(Void... voids) {
-            MediaPlayer mediaPlayer = ref.get().mediaPlayer;
-            ProgressBar progressBar = ref.get().progressBar;
             while (true) {
                 try {
                     Thread.sleep(100);
+                    publishProgress();
                 } catch (InterruptedException e) {
                     Logger.log(LOG_TAG, e);
                 }
-                if (!mediaPlayer.isPlaying()) {
-                    continue;
-                }
-                onProgressUpdate(progressBar.getMax() * mediaPlayer.getCurrentPosition()
-                        / mediaPlayer.getDuration());
             }
 //            return null;
         }
 
         @Override
-        protected void onProgressUpdate(Integer... values) {
+        protected void onProgressUpdate(Void... values) {
             super.onProgressUpdate(values);
-            int now = values[0];
-            ref.get().progressBar.setProgress(now);
+            MediaPlayer mediaPlayer = ref.get().mediaPlayer;
+            ProgressBar progressBar = ref.get().progressBar;
+            int now = progressBar.getMax() * mediaPlayer.getCurrentPosition() / mediaPlayer.getDuration();
+            progressBar.setProgress(now);
         }
     }
-
-    private ImageButton play, backward, forward;
     private MediaPlayer mediaPlayer;
     private ProgressBar progressBar;
     private ProgressSetter progressSetter;
@@ -75,22 +70,19 @@ public class SoundPlayer extends RelativeLayout {
     public SoundPlayer(Context context, AttributeSet attrs) {
         super(context, attrs);
         LayoutInflater.from(context).inflate(R.layout.sound_player, this);
-        play = findViewById(R.id.sound_player_play);
-        backward = findViewById(R.id.sound_player_backward);
-        forward = findViewById(R.id.sound_player_forward);
         mediaPlayer = new MediaPlayer();
         findViewById(R.id.sound_player_play).setOnClickListener(v -> {
             if (mediaPlayer.isPlaying()) {
                 mediaPlayer.pause();
-//                play.setBackgroundResource(R.drawable.ic_media_play);
             } else {
                 mediaPlayer.start();
-//                play.setImageResource(R.drawable.ic_media_pause);
             }
         });
         progressBar = findViewById(R.id.sound_player_progress);
         progressSetter = new ProgressSetter(this);
-        progressSetter.execute();
+        // set the progressSetter work on multi thread
+        // or only one progressSetter will be working
+        progressSetter.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 
     public void destroy() {
