726ee0dff82fa423842b85979653bcef89367479
ExfJoe <gwy17@mails.tsinghua.edu.cn>
2018-10-24 10:38:37 +0100

    On DeletableEditText :     add TextView named md to show markdown.     add function named setMarkdown() to init md.     add function named render() to render markdown. #26.

--- app/src/main/java/com/se/npe/androidnote/editor/DeletableEditText.java
+++ app/src/main/java/com/se/npe/androidnote/editor/DeletableEditText.java
@@ -1,22 +1,78 @@
 package com.se.npe.androidnote.editor;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
 import android.view.inputmethod.InputConnectionWrapper;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import com.yydcdut.markdown.MarkdownConfiguration;
 import com.yydcdut.markdown.MarkdownEditText;
+import com.yydcdut.markdown.MarkdownProcessor;
+import com.yydcdut.markdown.callback.OnTodoClickCallback;
+import com.yydcdut.markdown.loader.MDImageLoader;
+import com.yydcdut.markdown.syntax.text.TextFactory;
+import com.yydcdut.markdown.theme.ThemeSunburst;
 
 /**
  * 处理软键盘回删按钮backSpace时回调OnKeyListener
  */
 public class DeletableEditText extends MarkdownEditText {
+    private TextView md;
+    private MarkdownConfiguration markdownConfiguration;
+    private MarkdownProcessor processor;
 
     public DeletableEditText(Context context) {
         super(context);
         setPadding(0, 0, 0, 0);
+        md = new TextView(context);
+        setMarkdown(context);
+    }
+
+    private void setMarkdown(Context context) {
+        markdownConfiguration = new MarkdownConfiguration.Builder(context)
+                .setDefaultImageSize(50, 50)
+                .setBlockQuotesLineColor(0xff33b5e5)
+                .setHeader1RelativeSize(1.6f)
+                .setHeader2RelativeSize(1.5f)
+                .setHeader3RelativeSize(1.4f)
+                .setHeader4RelativeSize(1.3f)
+                .setHeader5RelativeSize(1.2f)
+                .setHeader6RelativeSize(1.1f)
+                .setHorizontalRulesColor(0xff99cc00)
+                .setCodeBgColor(0xffff4444)
+                .setTodoColor(0xffaa66cc)
+                .setTodoDoneColor(0xffff8800)
+                .setUnOrderListColor(0xff00ddff)
+                .setHorizontalRulesHeight(1)
+                .setLinkFontColor(Color.BLUE)
+                .showLinkUnderline(false)
+                .setTheme(new ThemeSunburst())
+                .setOnTodoClickCallback(new OnTodoClickCallback() {
+                    @Override
+                    public CharSequence onTodoClicked(View view, String line, int lineNumber) {
+                        return md.getText();
+                    }
+                })
+                .build();
+        processor = new MarkdownProcessor(context);
+        processor.factory(TextFactory.create());
+        processor.config(markdownConfiguration);
+    }
+
+    public void render(boolean isRender) {
+        if (isRender) {
+            md.setText(processor.parse(getText()));
+            md.setVisibility(VISIBLE);
+            this.setVisibility(GONE);
+        } else {
+            md.setVisibility(GONE);
+            this.setVisibility(VISIBLE);
+        }
     }
 
     @Override
@@ -43,5 +99,12 @@ public class DeletableEditText extends MarkdownEditText {
             return super.deleteSurroundingText(beforeLength, afterLength);
         }
 
+        public TextView getMd() {
+            return md;
+        }
+
+        public void setMd(TextView md) {
+            this.md = md;
+        }
     }
 }
