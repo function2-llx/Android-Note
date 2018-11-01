492d9862b6dd6d0073d030a6bb71a4e15a50dc92
mango <weijd17@mails.tsinghua.edu.cn>
2018-11-01 21:56:17 +0800

    Get ready to test NoteAdapter #34 Add test for permission

---
+++ app/src/test/java/com/se/npe/androidnote/PermissionTest.java
@@ -0,0 +1,14 @@
+package com.se.npe.androidnote;
+
+import android.app.Application;
+
+import org.robolectric.Shadows;
+import org.robolectric.shadows.ShadowApplication;
+
+public class PermissionTest {
+
+    public static void grantPermission(final Application app, final String permission) {
+        ShadowApplication shadowApp = Shadows.shadowOf(app);
+        shadowApp.grantPermissions(permission);
+    }
+}
