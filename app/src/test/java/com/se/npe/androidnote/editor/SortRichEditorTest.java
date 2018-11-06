d5c956e45a19ef5182408576dde2cfe13fec4ad6
ExfJoe <gwy17@mails.tsinghua.edu.cn>
2018-11-06 16:10:19 +0800

    create test file : SortRichEditorTest.java. #55

---
+++ app/src/test/java/com/se/npe/androidnote/editor/SortRichEditorTest.java
@@ -0,0 +1,62 @@
+package com.se.npe.androidnote.editor;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SortRichEditorTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void sort() {
+    }
+
+    @Test
+    public void changeIsMarkdown() {
+    }
+
+    @Test
+    public void showOrHideKeyboard() {
+    }
+
+    @Test
+    public void addPicture() {
+    }
+
+    @Test
+    public void addVideo() {
+    }
+
+    @Test
+    public void addSound() {
+    }
+
+    @Test
+    public void destroy() {
+    }
+
+    @Test
+    public void loadNote() {
+    }
+
+    @Test
+    public void buildNote() {
+    }
+
+    @Test
+    public void setViewOnly() {
+    }
+
+    @Test
+    public void setMarkdownController() {
+    }
+}
