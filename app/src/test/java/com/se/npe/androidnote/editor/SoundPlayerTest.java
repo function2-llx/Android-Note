c4aeef1e7155e82b21520f3a4e1cc8f8bcf82d73
ExfJoe <gwy17@mails.tsinghua.edu.cn>
2018-11-12 14:52:34 +0800

    create test file : SoundPlayerTest.java. #57

---
+++ app/src/test/java/com/se/npe/androidnote/editor/SoundPlayerTest.java
@@ -0,0 +1,25 @@
+package com.se.npe.androidnote.editor;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SoundPlayerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void destroy() {
+    }
+
+    @Test
+    public void setSource() {
+    }
+
+    @Test
+    public void getEditText() {
+    }
+}
