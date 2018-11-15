fdbfae10c27b9da6423ad23d962a2d4b0fc17bbf
潘俊臣 <2017011475@secoder.net>
2018-11-15 10:21:45 +0800

    Try to fix test fail

--- app/src/test/java/com/se/npe/androidnote/models/TableOperateTest.java
+++ app/src/test/java/com/se/npe/androidnote/models/TableOperateTest.java
@@ -96,6 +96,17 @@ public class TableOperateTest {
     }
 
     @Test
+    public void getAllTags() {
+        Note tempNote = new Note();
+        tempNote.getTag().add("Test_Tag");
+        assertEquals(20,TableOperate.getInstance().getAllTags().size());
+        TableOperate.getInstance().addNote(tempNote);
+        assertEquals(21,TableOperate.getInstance().getAllTags().size());
+        TableOperate.getInstance().removeNote(tempNote);
+        assertEquals(20,TableOperate.getInstance().getAllTags().size());
+    }
+
+    @Test
     public void getAllNotes() {
         List<String> tagList = new ArrayList<>();
         tagList.add("TagZ");
