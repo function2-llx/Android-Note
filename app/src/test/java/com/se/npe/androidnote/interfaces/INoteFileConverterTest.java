95189655ef3f8a98c6d835b085828cd0c820b376
mango <weijd17@mails.tsinghua.edu.cn>
2018-11-15 10:51:48 +0800

    Refactor INoteFileConverter and its test #63 Use ReturnValueEater to supress sonarqube

--- app/src/test/java/com/se/npe/androidnote/interfaces/INoteFileConverterTest.java
+++ app/src/test/java/com/se/npe/androidnote/interfaces/INoteFileConverterTest.java
@@ -3,6 +3,8 @@ package com.se.npe.androidnote.interfaces;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 
+import com.se.npe.androidnote.models.TableOperate;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,17 +14,18 @@ import org.robolectric.RobolectricTestRunner;
 import java.io.File;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(RobolectricTestRunner.class)
 public class INoteFileConverterTest {
 
-    private Context context;
     private static final String FILE_NAME = "test";
 
     @Before
     public void setUp() {
         AppCompatActivity activity = Robolectric.setupActivity(AppCompatActivity.class);
-        context = activity.getApplicationContext();
+        Context context = activity.getApplicationContext();
+        TableOperate.init(context); // initialize TableConfig.SAVE_PATH
     }
 
     @Test
@@ -30,10 +33,10 @@ public class INoteFileConverterTest {
         INoteFileConverter.createFileToExport(FILE_NAME);
         // successfully create dir
         File exportDir = new File(INoteFileConverter.getExportDirPath());
-        assertNotNull(exportDir);
+        assertTrue(exportDir.exists());
         // successfully create file
         File exportFile = new File(INoteFileConverter.getExportFilePath(FILE_NAME));
-        assertNotNull(exportFile);
+        assertTrue(exportFile.exists());
         assertNotNull(exportDir.listFiles());
     }
 }
