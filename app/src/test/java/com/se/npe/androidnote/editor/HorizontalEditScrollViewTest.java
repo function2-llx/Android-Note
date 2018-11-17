60882b14ed8713522935fb30d52b4be9c73f07ef
ExfJoe <gwy17@mails.tsinghua.edu.cn>
2018-11-17 15:22:52 +0800

    edit test function : HorizontalEditScrollView.java/setUp() and onLongClick(). #65

--- app/src/test/java/com/se/npe/androidnote/editor/HorizontalEditScrollViewTest.java
+++ app/src/test/java/com/se/npe/androidnote/editor/HorizontalEditScrollViewTest.java
@@ -1,22 +1,32 @@
 package com.se.npe.androidnote.editor;
 
+import android.view.View;
+
+import com.se.npe.androidnote.EditorActivity;
+import com.se.npe.androidnote.R;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
 
 import static org.junit.Assert.*;
 
+@RunWith(RobolectricTestRunner.class)
 public class HorizontalEditScrollViewTest {
+    private HorizontalEditScrollView markdownController = null;
+    private SortRichEditor editor = null;
+    private EditorActivity activity = null;
 
     @Before
     public void setUp() throws Exception {
-    }
-
-    @Test
-    public void setEditTextAndConfig() {
-    }
-
-    @Test
-    public void onFinishInflate() {
+        activity = Robolectric.setupActivity(EditorActivity.class);
+        assertNotNull(activity);
+        editor = activity.findViewById(R.id.rich_editor);
+        assertNotNull(editor);
+        markdownController = activity.findViewById(R.id.scroll_edit);
+        assertNotNull(markdownController);
     }
 
     @Test
@@ -25,5 +35,6 @@ public class HorizontalEditScrollViewTest {
 
     @Test
     public void onLongClick() {
+        markdownController.onLongClick(activity.findViewById(R.id.img_block_quote));
     }
 }
