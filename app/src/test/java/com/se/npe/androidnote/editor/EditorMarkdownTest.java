8c3be1c6491f5bdc07c435a9010526498e1176d3
MashPlant <740678788@qq.com>
2018-11-22 00:21:08 +0800

    Working on test for markdone, code & style & block quote already finished. #68

--- app/src/test/java/com/se/npe/androidnote/editor/EditorMarkdownTest.java
+++ app/src/test/java/com/se/npe/androidnote/editor/EditorMarkdownTest.java
@@ -2,6 +2,7 @@ package com.se.npe.androidnote.editor;
 
 import com.se.npe.androidnote.EditorActivity;
 import com.se.npe.androidnote.R;
+import com.yydcdut.markdown.MarkdownEditText;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,25 +14,50 @@ import static org.junit.Assert.assertNotNull;
 
 @RunWith(RobolectricTestRunner.class)
 public class EditorMarkdownTest {
-    private SortRichEditor editor;
     private EditorActivity activity;
+    private HorizontalEditScrollView markdownController;
+    private MarkdownEditText editText;
 
     @Before
     public void setUp() {
         activity = Robolectric.setupActivity(EditorActivity.class);
         assertNotNull(activity);
-        editor = activity.findViewById(R.id.rich_editor);
+        SortRichEditor editor = activity.findViewById(R.id.rich_editor);
         assertNotNull(editor);
+        markdownController = activity.findViewById(R.id.scroll_edit);
+        assertNotNull(markdownController);
+        editor.emptyView.performClick();
+        editText = (MarkdownEditText) (editor.lastFocusEdit);
     }
 
     @Test
     public void testBlockQuotesController() {
-
+        editText.setText("hello");
+        editText.setSelection(0, 5);
+        markdownController.onLongClick(activity.findViewById(R.id.img_block_quote));
+        editText.setSelection(0, 0);
+        markdownController.onLongClick(activity.findViewById(R.id.img_block_quote));
+        editText.setSelection(0, 5);
+        markdownController.onClick(activity.findViewById(R.id.img_block_quote));
+        editText.setSelection(0, 0);
+        markdownController.onClick(activity.findViewById(R.id.img_block_quote));
     }
 
     @Test
     public void testStyleController() {
-
+        editText.setText("hello");
+        editText.setSelection(0, 5);
+        markdownController.onClick(activity.findViewById(R.id.img_bold));
+        editText.setSelection(0, 1);
+        markdownController.onClick(activity.findViewById(R.id.img_bold));
+        editText.setSelection(0, 0);
+        markdownController.onClick(activity.findViewById(R.id.img_bold));
+        editText.setSelection(0, 5);
+        markdownController.onClick(activity.findViewById(R.id.img_italic));
+        editText.setSelection(0, 1);
+        markdownController.onClick(activity.findViewById(R.id.img_italic));
+        editText.setSelection(0, 0);
+        markdownController.onClick(activity.findViewById(R.id.img_italic));
     }
 
     @Test
@@ -41,7 +67,24 @@ public class EditorMarkdownTest {
 
     @Test
     public void testCodeController() {
-
+        String code = "#include<cstdio>\n"
+                + "int main()\n"
+                + "{\n"
+                + "    printf(\"hello world\\n\");\n"
+                + "}\n";
+        editText.setText(code);
+        editText.setSelection(0, code.length());
+        markdownController.onClick(activity.findViewById(R.id.img_code));
+        editText.setSelection(0, 0);
+        markdownController.onClick(activity.findViewById(R.id.img_code));
+        editText.setSelection(0, code.length());
+        markdownController.onClick(activity.findViewById(R.id.img_inline_code));
+        editText.setSelection(0, 0);
+        markdownController.onClick(activity.findViewById(R.id.img_inline_code));
+        code = "print('hello world')";
+        editText.setText(code);
+        editText.setSelection(0, code.length());
+        markdownController.onClick(activity.findViewById(R.id.img_inline_code));
     }
 
     @Test
